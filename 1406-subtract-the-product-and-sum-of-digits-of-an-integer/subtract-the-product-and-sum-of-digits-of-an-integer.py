e0f74a463605db70a68bcbb37c4ebac5b9fb2984
Husain Hakim <142593070+husainhakim@users.noreply.github.com>
2024-05-03 14:32:30 +0530

    Time: 41 ms (14.57%) | Memory: 16.5 MB (40.82%) - LeetSync

---
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.py
@@ -0,0 +1,9 @@
+class Solution:
+    def subtractProductAndSum(self, n) -> int:
+        multiple=1
+        sumofnum=0
+        for i in str(n):
+            multiple*=int(i)
+            sumofnum+=int(i)
+        difference=multiple-sumofnum
+        return difference
